e699fb801bb3da281e843de751cf358529316932
Daniel Millan <dani.millan07@gmail.com>
2024-02-08 17:30:47 -0500

    Initial commit

---
+++ apps/ocmi-dmillan-app-next-web-ui-e2e/cypress.config.ts
@@ -0,0 +1,15 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run ocmi-dmillan-app-next-web-ui:start',
+      },
+    }),
+    baseUrl: 'http://localhost:3000',
+  },
+});
---
+++ apps/ocmi-dmillan-app-next-web-ui/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'ocmi-dmillan-app-next-web-ui',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/ocmi-dmillan-app-next-web-ui',
+};
